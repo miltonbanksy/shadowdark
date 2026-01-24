736572a762f656e71b08ee1f2e1f04bf386339ac
miltonbanksy <miltonbanksy@gmail.com>
2026-01-24 17:05:00 +0900

    Before CGPT help

--- script.js
+++ script.js
@@ -12,61 +12,8 @@ fetch('ancestry.json').then(res => res.json()).then(data => ancestryData = data)
 fetch('statsAndMods.json').then(res => res.json()).then(data => statsAndMods = data);
 
 
-// Breaking this function down... down!
-function generateStatArray(numberOfCharacters, numberOfStats, numberOfDice) {
-    
-        
-    const characterCard = document.createElement('div');
-    characterCard.classList.add('character-card');
-
-    characterCard.innerHTML = `<h3>Level 0 ${ancestry.ancestry}</h3>`;
-
-    const table = document.createElement('table');
-
-    statNames.forEach((statName, index) => {
-        
-        const statValue = allStatsBag[index];
-        const mod = getModifier(statValue);
-
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${statName}</td>
-            <td>${statValue}</td>
-            <td>(${mod >= 0 ? '+' : ''}${mod})</td>
-        `;
-        table.appendChild(row);
-    });
-
-    const ancestryDescription = document.createElement('p');
-    ancestryDescription.innerHTML = ancestry.notes;
-
-    characterCard.appendChild(table);
-    characterCard.appendChild(ancestryDescription);
-    displayCharacters.appendChild(characterCard);
-    }
-
 function getModifier(stat) {
     return statsAndMods.find(s => s.stat === stat).mod;
-}
-
-function displayArrayOfObjects(array) {
-    displayCharacters.innerHTML = '';
-
-    array.forEach(item => {
-        const itemDiv = document.createElement('div');
-        itemDiv.classList.add('item');
-
-        let content = '';
-        for (const key in item) {
-            if (Object.hasOwnProperty.call(item, key)) {
-                const value = item[key];
-                content += `<strong>${key}:</strong> ${value}<br>`;
-            }
-        }
-        itemDiv.innerHTML = content;
-
-        displayCharacters.appendChild(itemDiv);
-    })
 }
 
 function roll_1dx(dieSize) { // Roll a single polyhedral die
@@ -80,7 +27,7 @@ function generateStats() {
             let oneStatBag = [];
             
             for ( d = 1; d <= 3; d++ ) { // roll 3 dice for each stat
-                const threeDice = roll_1dx(3);
+                const threeDice = roll_1dx(6);
                 oneStatBag.push(threeDice);
             }
             
@@ -99,21 +46,56 @@ function generateAncestry() {
     return ancestry;
 }
 
+function generateHitPoints() {
+
+}
+
+
+// LISTENERS
+
 buttonNumberOfCharacters.addEventListener('click', () => {
     
     displayCharacters.innerHTML = '';
-    createDisplay();
 
     const numberOfCharacters = Number(inputNumberOfCharacters.value);
     
+    let ancestry = "";
     let ancestryBag = [];
     
-    //generateStatArray(number_of_characters, 6, 3);
-
     for (c = 1; c <= numberOfCharacters; c++ ) {
-        let ancestry = generateAncestry(roll_1dx(12));
+
+        ancestry = generateAncestry();
         ancestryBag.push(ancestry);
         allStatsBag = generateStats();
+
+        const characterCard = document.createElement('div');
+        characterCard.classList.add('character-card');
+
+        const cardTitle = document.createElement('h3');
+        
+        const table = document.createElement('table');
+
+        statNames.forEach((statName, index) => {
+
+            const statValue = allStatsBag[index];
+            const mod = getModifier(statValue);
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${statName}</td>
+                <td>${statValue}</td>
+                <td>(${mod >= 0 ? '+' : ''}${mod})</td>
+            `;
+            table.appendChild(row);
+        });
+        cardTitle.innerHTML = `Level 0 ${ancestry.ancestry}`;
+        const cardNotes = document.createElement('p');
+        cardNotes.innerHTML = ancestry.notes;
+
+        characterCard.appendChild(cardTitle);
+        characterCard.appendChild(table);
+        characterCard.appendChild(cardNotes);
+        displayCharacters.appendChild(characterCard);
         console.log(allStatsBag);
     }
 });
